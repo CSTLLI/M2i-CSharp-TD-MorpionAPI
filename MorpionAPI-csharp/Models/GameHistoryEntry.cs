a7e8970604f72cba57c0445abd3daedee1d425a1
CASTELLI Valerio <78420708+CSTLLI@users.noreply.github.com>
2026-05-20 09:11:29 +0200

    Revert "feat: init project"
    This reverts commit 035102a0698f5a3e9790322f484ce13575f48e1d.

--- MorpionAPI-csharp/Models/GameHistoryEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace MorpionAPI_csharp.Models;
-
-public class GameHistoryEntry
-{
-    public string Result { get; set; } = "";
-    public string Date { get; set; } = "";
-    public Guid GameId { get; set; }
-    public string DisplayText => $"{Date} - {Result}";
-}
--- MorpionAPI-csharp/Services/IGameHistoryService.cs
+++
@@ -1,9 +0,0 @@
-using MorpionAPI_csharp.Models;
-
-namespace MorpionAPI_csharp.Services;
-
-public interface IGameHistoryService
-{
-    IReadOnlyList<GameHistoryEntry> History { get; }
-    void Add(string result, Guid gameId);
-}
